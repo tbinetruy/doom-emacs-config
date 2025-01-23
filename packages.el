f625a24aa8061fa2ba2475d27df8819fd5e9d189
Thomas Binetruy <tbin@pyratzlabs.com>
2025-01-23 10:53:30 +0100

    feat: removed reason-mode package.

--- packages.el
+++ packages.el
@@ -8,6 +8,7 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
+;(package! reason-mode)
 (package! rescript-mode)
 (package! lsp-rescript)
 (package! ob-mermaid)
